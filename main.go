41f80ffff4724ff0419477ba4493547927b55f85
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2017-07-11 16:07:50 +0900

    don't send real-path

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bufio"
+	"crypto/md5"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
@@ -9,15 +11,19 @@ import (
 	"log"
 	"net"
 	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 )
+
+var hashes = make(map[string]string)
 
 func sendFile(conn net.Conn, filename string) error {
 	f, err := os.Open(filename)
 	if err != nil {
 		return err
 	}
+
 	defer f.Close()
 
 	st, err := f.Stat()
@@ -25,12 +31,15 @@ func sendFile(conn net.Conn, filename string) error {
 		return err
 	}
 
+	hash := fmt.Sprintf("%x", md5.Sum([]byte(filename)))
+	hashes[hash] = filename
+
 	fmt.Fprintf(conn, "open\n")
-	fmt.Fprintf(conn, "display-name: %v\n", filename)
-	fmt.Fprintf(conn, "real-path: %v\n", filename)
+	fmt.Fprintf(conn, "display-name: %v\n", filepath.Base(filename))
+	//fmt.Fprintf(conn, "real-path: %v\n", filename)
 	fmt.Fprintf(conn, "data-on-save: yes\n")
 	fmt.Fprintf(conn, "re-activate: yes\n")
-	fmt.Fprintf(conn, "token: %v\n", filename)
+	fmt.Fprintf(conn, "token: %v\n", hash)
 	fmt.Fprintf(conn, "data: %v\n", st.Size())
 	io.Copy(conn, f)
 	fmt.Fprintf(conn, "\n.\n")
@@ -49,16 +58,17 @@ func handleCmds(buf *bufio.Reader) error {
 	case "close":
 		return nil
 	case "save":
-		var filename string
+		var token string
 		var size int64
 		for {
 			b, _, err = buf.ReadLine()
 			if err != nil {
 				return err
 			}
-			cmd = string(b)
+			cmd = strings.TrimSpace(string(b))
+			log.Printf(cmd)
 			if strings.HasPrefix(cmd, "token:") {
-				filename = strings.TrimSpace(cmd[6:])
+				token = strings.TrimSpace(cmd[6:])
 			} else if strings.HasPrefix(cmd, "data:") {
 				size, err = strconv.ParseInt(cmd[6:], 10, 64)
 				if err != nil {
@@ -77,7 +87,10 @@ func handleCmds(buf *bufio.Reader) error {
 			return err
 		}
 		f.Close()
-		return os.Rename(f.Name(), filename)
+		if filename, ok := hashes[token]; ok {
+			return os.Rename(f.Name(), filename)
+		}
+		return errors.New("unknown token: " + token)
 	}
 	return nil
 }
@@ -108,7 +121,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println(string(b))
+	log.Println(strings.TrimSpace(string(b)))
 	for {
 		handleCmds(buf)
 	}
